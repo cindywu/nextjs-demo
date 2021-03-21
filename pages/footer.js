4dbd1fc8c726154f917134cc997604e1124173cb
cindy <ciindy.wu@gmail.com>
2021-03-21 12:11:24 -1000

    Fix heart padding, underline links

--- pages/footer.js
+++ pages/footer.js
@@ -1,10 +1,37 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
     <div className="footer-text">
-      <p> Powered by GPT-3. Results may not be perfect.</p>
-      <p>Made with &#10084;&#65039; by <a href="https://twitter.com/itsyashdani">Yash</a> & <a href="https://twitter.com/cindywu">Cindy</a>.</p>
-      <p>Follow <a href="https://twitter.com/tldrpapers">@tldrpapers</a> to see trending papers summarized.</p>
-      <p className="disclaimer">This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply. Max 50 summaries a day, contact us for more.</p>
+      Powered by GPT-3. Results may not be perfect.
+      <br/>
+      Made with{' '} 
+      <span className="heart">&#10084;&#65039;</span>
+      by{' '}
+        Yash 
+      </Link> 
+      {''} & {''} 
+      <Link href="https://twitter.com/cindywu">
+        Cindy
+      </Link>
+      .
+      <br/>
+      Follow{' '}  
+      <Link href="https://twitter.com/tldrpapers">
+        @tldrpapers
+      </Link>
+      {' '} to see trending papers summarized.
+      <p className="disclaimer">
+        This site is protected by reCAPTCHA and the Google{' '}
+        <Link href="https://policies.google.com/privacy">
+          Privacy Policy
+        </Link>
+        {' '}and{' '}
+        <Link href="https://policies.google.com/terms">
+          Terms of Service
+        </Link>
+        {' '}apply. Max 50 summaries a day, contact us for more.
+      </p>
     </div>
   )
 }
